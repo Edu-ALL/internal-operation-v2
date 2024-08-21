369733e9a444855d626b779fa5f1d9f5162b4677
all-inedu <all.in.eduspace@gmail.com>
2024-08-21 09:23:58 +0700

    update

--- app/Repositories/ClientRepository.php
+++ app/Repositories/ClientRepository.php
@@ -1853,7 +1853,7 @@ class ClientRepository implements ClientRepositoryInterface
                 break;
 
             case 'parent':
-                $relation = ['school', 'childrens'];
+                $relation = ['school', 'childrens', 'clientProgram.program', 'parents'];
 
                 break;
 
