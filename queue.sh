49c7ab90e0ccb5f6dab7f344a9c7496ea41de5a6
all-inedu <all.in.eduspace@gmail.com>
2024-09-12 14:32:43 +0700

    add queue send-hold-program

--- queue.sh
+++ queue.sh
@@ -4,6 +4,6 @@
 if ! pgrep -f "queue:listen" > /dev/null; then
     # Start the process and redirect output
     /usr/local/bin/php /home/u5794939/public_html/artisan queue:listen \
-        --queue=default,inv-send-to-client,inv-email-request-sign,verifying-client,verifying-client-parent,verifying-client-teacher,imports-student,imports-parent,imports-teacher,imports-client-event,imports-school-merge,verifying_client,verifying_client_parent,verifying_client_teacher,define-category-client,get-took-ia \
+        --queue=default,inv-send-to-client,inv-email-request-sign,verifying-client,verifying-client-parent,verifying-client-teacher,imports-student,imports-parent,imports-teacher,imports-client-event,imports-school-merge,verifying_client,verifying_client_parent,verifying_client_teacher,define-category-client,get-took-ia,send-hold-program \
         >> /home/u5794939/public_html/storage/logs/queue_output.log 2>&1
 fi
